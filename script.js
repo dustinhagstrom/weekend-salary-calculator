649d8d617c4e608fc2c7439a0a4c8dcea4d3819a
Dustin Hagstrom <dustnhgstrm@gmail.com>
2023-12-02 20:15:19 -0600

    made function signatures and grabbed dom input elements and the submit button.

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("hello javascript!");
+// console.log("hello javascript!");
+
+// get the tbody
+let tbody = document.querySelector("table");
+
+// get the input fields
+let fName = document.getElementById("first-name");
+let lName = document.getElementById("last-name");
+let id = document.getElementById("id");
+let title = document.getElementById("title");
+let salary = document.getElementById("salary");
+
+// console.log input objects
+console.log("input field obj fName: ", fName);
+console.log("input field obj lName: ", lName);
+console.log("input field obj id: ", id);
+console.log("input field obj title: ", title);
+console.log("input field obj salary: ", salary);
+
+// this function will process the taking of input data
+// and creating a new row in the table.
+function submitHandler(){
+
+}
+
+// this function will delete the row with which the pressed
+// delete button is associated with.
+function deleteHandler(){
+
+}
+
+// this function will loop through the row entries in the
+// table and it will tabulate the total annual salaries
+// and put that data into a text box.
+function calculateTotal(){
+
+}
